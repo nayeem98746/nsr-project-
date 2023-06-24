1faa3cf45e1c63e86fe97905376a378b839c2ec0
itsrazib1 <itsrazib1@gmail.com>
2023-06-24 17:09:46 +0600

    added frist

--- src/components/navber/Navber.jsx
+++ src/components/navber/Navber.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import style from './navber.module.css'
+import React from "react";
+import style from "./navber.module.css";
 
 const Navber = () => {
-    return (
-        <div>
-            <h2 className={style.main}>navber </h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className={style.main}>navber </h2>
+      <div>
+        <ul>
+          <li>NSR</li>
+          <li>Shop</li>
+          <li>Product</li>
+          <li>Blog</li>
+          <li>Pages</li>
+          <li>New In</li>
+          <li>Trend</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navber;
