d2eb999f7d3ce1c8e4ba3ee3a4d67bffc9eaa253
nayeem98746 <sabbirahmednayeem941@gmail.com>
2023-06-24 16:44:54 +0600

    changes

---
+++ src/components/navber/Navber.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './navber.module.css'
+
+const Navber = () => {
+    return (
+        <div>
+            <h2 className={style.main}>navber </h2>
+        </div>
+    );
+};
+
+export default Navber;
